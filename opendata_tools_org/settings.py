dea9656c629ae0ce532a07315de17112daf15992
holgerd77 <Holger.Drewes@googlemail.com>
2012-06-28 13:41:16 +0200

    Added Open Data badge

--- opendata_tools_org/settings.py
+++ opendata_tools_org/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'haystack',
     'website_showroom',
+    'opendata_tools_org',
 )
 
 # A sample logging configuration. The only tangible logging
