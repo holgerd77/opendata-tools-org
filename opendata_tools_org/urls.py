9b43b2734f40a31acbe8054c6bc861afe9684fd2
holgerd77 <Holger.Drewes@googlemail.com>
2012-07-03 17:52:03 +0200

    Moved url patterns to django-website-showroom

--- opendata_tools_org/urls.py
+++ opendata_tools_org/urls.py
@@ -1,23 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from website_showroom.feeds import RssFeed
+from website_showroom.urls import urlpatterns
 
-from django.contrib import admin
-admin.autodiscover()
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^rss/$', RssFeed()), 
-    url(r'^search/', include('haystack.urls')),
-
-    url(r'^$', 'website_showroom.views.index'),
-    url(r'^contact/', 'website_showroom.views.contact'),
-    url(r'^(?P<url_name>[-\w]+)/$', 'website_showroom.views.category')
-)
 
 if settings.DEBUG:
     urlpatterns += patterns('',
