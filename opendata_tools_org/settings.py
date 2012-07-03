4c77b50c340a7ed8e2f664c7ddf45150ad978616
holgerd77 <Holger.Drewes@googlemail.com>
2012-07-03 17:56:12 +0200

    Minor changes

--- opendata_tools_org/settings.py
+++ opendata_tools_org/settings.py
@@ -91,16 +91,6 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '*o$$q-*u#l#k56ex**sv4_fq&amp;j@%cud#@)@9irh0pqz0qn3(a!'
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-    'website_showroom.helper.context_processors.showroom',
-)
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
