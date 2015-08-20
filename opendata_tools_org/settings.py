6ae6bb395a8979ee4f9456518112623fed4a81f4
holgerd77 <Holger.Drewes@googlemail.com>
2015-08-20 13:44:49 +0200

    Added ALLOWED_HOSTS setting to settings.py

--- opendata_tools_org/settings.py
+++ opendata_tools_org/settings.py
@@ -28,6 +28,8 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['.opendata-tools.org',]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
