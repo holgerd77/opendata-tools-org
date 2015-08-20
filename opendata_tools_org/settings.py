80e01b161c14a0ca22247cf49729b03c0a6172f3
holgerd77 <Holger.Drewes@googlemail.com>
2015-08-20 15:00:13 +0200

    Switched from PROJECT_ROOT to BASE_DIR in settings.py to follow Django conventions

--- opendata_tools_org/settings.py
+++ opendata_tools_org/settings.py
@@ -9,7 +9,7 @@ else:
     DEBUG = False
 
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, 'sqlite3.db'), # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, 'opendata_tools_org', 'sqlite3.db'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -58,7 +58,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'opendata_tools_org', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -69,7 +69,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -168,7 +168,7 @@ LOGGING = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(PROJECT_ROOT, 'search_index'),
+        'PATH': os.path.join(BASE_DIR, 'opendata_tools_org', 'search_index'),
     },
 }
 
